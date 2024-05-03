5e5ed39fd492fd0ece88a05d72b6150893659638
Nikhil <nikhildhiman9050@gmail.com>
2024-05-03 21:07:58 +0530

    fix

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from instaloader import Instaloader, Profile
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/fetch_reels/<username>', methods=['GET'])
 def fetch_reels(username):
